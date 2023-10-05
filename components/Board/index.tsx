edf1b75e83932e048f2887d467458cd547793400
krutulkamil <krutulkamil@onet.pl>
2023-10-05 16:37:54 +0200

    feat: same column drag

--- components/Board/index.tsx
+++ components/Board/index.tsx
@@ -13,7 +13,7 @@ export function Board() {
 
   useEffect(() => {
     getBoard();
-  }, []);
+  }, [getBoard]);
 
   function handleDragEnd(result: DropResult) {
     const { destination, source, type } = result;
@@ -48,8 +48,6 @@ export function Board() {
       todos: finishColIndex[1].todos,
     };
 
-    console.log(startCol, finishCol)
-
     if (!startCol || !finishCol) return;
     if (source.index === destination.index && startCol === finishCol) return;
 
@@ -65,10 +63,24 @@ export function Board() {
       const newColumns = new Map(board.columns);
       newColumns.set(startCol.id, newCol);
 
-      setBoardState({ ...board, columns: newColumns })
+      setBoardState({ ...board, columns: newColumns });
     } else {
-      const newFinishTodos = finishCol.todos;
-      newFinishTodos.splice(destination.index, 0, todoMoved);
+      const finishTodos = Array.from(finishCol.todos);
+      finishTodos.splice(destination.index, 0, todoMoved);
+
+      const newColumns = new Map(board.columns);
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+
+      newColumns.set(startCol.id, newCol);
+      newColumns.set(finishCol.id, {
+        id: finishCol.id,
+        todos: finishTodos,
+      });
+
+      setBoardState({ ...board, columns: newColumns });
     }
   }
 
